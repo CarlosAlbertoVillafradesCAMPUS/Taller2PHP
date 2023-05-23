cde458a93edcdbf1f9dbc30d524ca0df52851b44
CarlosAlbertoVillafradesCAMPUS <cavillafrades@gmail.com>
2023-05-23 17:10:25 -0500

    feat(Ejercicios): :rocket: Ejercicio 6
    se desarrollo el ejercicio 6 del taller 2, sobre calcular la nota mayor y menor de uns estudiantes . se validaron la gran mayoria anomalias tanto en el frontend como en el backend
    
    by: Carlos Villafrades

--- api.php
+++ api.php
@@ -2,65 +2,71 @@
 header("Content-Type:application/json");
 
 $_DATA = json_decode(file_get_contents("php://input"), true);
+
 $_METHOD = $_SERVER["REQUEST_METHOD"];
 var_dump($_METHOD);
 
-function sumAndRest($numero1, $numero2){
-    $sum = $numero1 + $numero2;
-    $rest = $numero1 - $numero2;
+function calcMayorNota($arg){
+        $arrayNotas = [];
+        $cantidadHombres= (int) 0;
+        $cantidadMujeres= (int) 0;
+        for ($i=0; $i < count($arg) ; $i++) { 
+            if (!is_string($arg[$i]["nombre"]) || !is_numeric($arg[$i]["nota"])) {
+                return(array)[
+                    "Maxima" => "Error!!",
+                    "Minima" => "Error!!",
+                    "cantHombres" => "Error!!",
+                    "cantMujeres" => "Error!!"
+                ];
+                break;
+            }else{ 
+                $nota = (float)$arg[$i]["nota"];
+                array_push($arrayNotas, $nota);
+                
+                if ($arg[$i]["sexo"] == "M") {
+                    $cantidadHombres++;
+                } else {
+                    $cantidadMujeres++;
+                }
+            } 
+        }
+        $notaMaxima = max($arrayNotas);
+        $notaMinima = min($arrayNotas);
+        $positionNotaMenor = array_search($notaMinima, $arrayNotas);
+        $positionNotaMayor = array_search($notaMaxima, $arrayNotas);
+        var_dump($cantidadHombres);
 
-    return (array)[
-        "sum"=>$sum,
-        "rest"=>$rest
-    ];
-}
-
-function multAndDiv($numero1, $numero2){
-    $mult = $numero2 * $numero1;
-    $div = $numero2 / $numero1;
-
-    return (array)[
-        "mult"=>$mult,
-        "div"=>$div
-    ];
-}
-
-function calcNumber(float $numero1, float $numero2){
-    if (!is_numeric($numero1) || !is_numeric($numero2) ) {
-        return "Error!!";
-    }else{
-        return ($numero1 > $numero2) ?sumAndRest($numero1, $numero2) :multAndDiv($numero1, $numero2);
-    }
+        return(array)[
+            "Maxima" => $arg[$positionNotaMayor],
+            "Minima" => $arg[$positionNotaMenor],
+            "cantHombres" => $cantidadHombres,
+            "cantMujeres" => $cantidadMujeres
+        ];
 }
 
 try {
     $res = match($_METHOD){
-        "POST" => calcNumber(...$_DATA),
+        "POST" => calcMayorNota($_DATA),
         default => <<<STRING
         Methodo "${_METHOD}" imposible de ejecutar, rectifique su methodo
         STRING
     };
 } catch (\Throwable $th) {
     $res = (array)[
-        "sum"=>"Error!!",
-        "rest"=>"Error!!",
-    ];
-}
-
-if($res["sum"]){
-    $respuesta = (array) [
-        "data" => $_DATA,
-        "suma" => $res["sum"],
-        "resta" => $res["rest"],
-    ];
-}else{
-    $respuesta = (array) [
-        "data" => $_DATA,
-        "mult" => $res["mult"],
-        "div" => $res["div"],
+        "Maxima" => "Error!!",
+        "Minima" => "Error!!",
+        "cantHombres" => "Error!!",
+        "cantMujeres" => "Error!!"
     ];
 }
 
 
-echo json_encode($respuesta, JSON_PRETTY_PRINT);
+    $respuesta = (array) [
+        "notaMaxima" => $res["Maxima"],
+        "notaMinima" => $res["Minima"],
+        "cantidadHombres" => $res["cantHombres"],
+        "cantidadMujeres" => $res["cantMujeres"]
+    ];
+
+    echo json_encode($respuesta, JSON_PRETTY_PRINT);
 ?>
--- main.js
+++ main.js
@@ -4,22 +4,75 @@ diferencia, en caso contrario, informar el producto y la
 división del primero respecto al segundo. */
 
 let myForm = document.querySelector("#myForm");
+let data = [];
 
 myForm.addEventListener("submit", async (e) => {
     e.preventDefault();
     const dataForm = Object.fromEntries(new FormData(e.target));
-    if(dataForm.numero1 == "" || dataForm.numero2 == ""){
+    if(dataForm.numeroTotal == ""){
         alert("Error!! Porfavor llenar los campos");
     }else{
+        let datoCantidadEstudiantes = dataForm.numeroTotal;
+        
+        let fieldset = document.querySelector("fieldset")
+        fieldset.innerHTML = null;
+        fieldset.insertAdjacentHTML("beforeend", `
+                                                    <legend>Ejercicio 5</legend>
+                                                        <form id="myForm2">
+                                                            <label for="nombre">Ingrese el nombre del estudiantes: </label>
+                                                            <input id="nombre" type="text" name="nombre"/><br><br>
+                                                            
+                                                            <label for="nombre">Sexo del estudiante: </label>
+                                                            <select name="sexo">
+                                                                <option value="M">Masculino</option>
+                                                                <option value="F">Femenino</option>
+                                                            </select><br><br>
+
+                                                            <label for="nota">Ingrese la nota de estudiantes: </label>
+                                                            <input id="nota" type="number" name="nota"/><br>
+                                                            
+                                                            <input id="submit" type="submit" value="Agregar">
+                                                            <input id="botonCalcular" type="button" value="Calcular">
+                                                        </form>
+                                                  `);
+        formularioEstudiantes(datoCantidadEstudiantes);                                
+        
+    }
+});
+
+function formularioEstudiantes(arg){
+    let myForm2 = document.querySelector("#myForm2");
+    myForm2.addEventListener("submit", async(e) =>{
+        e.preventDefault();
+        const dataForm = Object.fromEntries(new FormData(e.target)); 
+
+        if(dataForm.nombre == "" || dataForm.nota == ""){
+            alert("Error!! Porfavor llenar los campos");
+        }else{
+            if(data.length <= (parseInt(arg)-1)){
+                data.push(dataForm);
+                if(data.length == arg){
+                    document.querySelector("#submit").setAttribute("disabled", "");
+                }
+            }else{
+                alert("Cantidad de estudiantes alcanzada");
+                
+            }
+        }
+        myForm2.reset();
+    })
+
+    document.querySelector("#botonCalcular").addEventListener("click", async(e) =>{
+        console.log(data);
         let config = {
             method: "POST",
             header: {"Content-Type":"application/json"},
-            body: JSON.stringify(dataForm)
+            body: JSON.stringify(data)
         }
         let res = await( await fetch("api.php", config)).text();
         document.querySelector("pre").innerHTML = res;
-    }
-});
+    })
+}
 
 
 
