2f7b505ed571eeb68786841fd087e641cf864781
CarlosAlbertoVillafradesCAMPUS <cavillafrades@gmail.com>
2023-05-23 14:47:20 -0500

    feat(Ejercicios): :rocket: Ejercicio 5
    se soluciono el ejercicio 5 del taller 2, sobre calcular entre 2 numero cual era el mayor y sumarlos restarlos o multiplicar y dividir. se siguio validando cualquier anomalia tanto en el frontend como en el backend
    
    by: Carlos Villafrades

--- api.php
+++ api.php
@@ -5,44 +5,62 @@ $_DATA = json_decode(file_get_contents("php://input"), true);
 $_METHOD = $_SERVER["REQUEST_METHOD"];
 var_dump($_METHOD);
 
-function calcMayor($arg){
-    if(count($arg) > 0 && count($arg) <= 3){
-        $i = (int) 0;
-        $sum = (int) 0;
-        for ($i=0; $i < count($arg); $i++) { 
-            if (!is_numeric($arg[$i]["edad"])) {
-                return "Error!!";
-                break;
-                }
-            $edad = (int) $arg[$i]["edad"];
-            if ($edad >= $sum) {
-                    $sum = (int) $arg[$i]["edad"];
-                    $position = $i;
-                }
-        }
+function sumAndRest($numero1, $numero2){
+    $sum = $numero1 + $numero2;
+    $rest = $numero1 - $numero2;
 
-        return $arg[$position];
-
-} else{
-    return "Error!!";
+    return (array)[
+        "sum"=>$sum,
+        "rest"=>$rest
+    ];
 }
+
+function multAndDiv($numero1, $numero2){
+    $mult = $numero2 * $numero1;
+    $div = $numero2 / $numero1;
+
+    return (array)[
+        "mult"=>$mult,
+        "div"=>$div
+    ];
+}
+
+function calcNumber(float $numero1, float $numero2){
+    if (!is_numeric($numero1) || !is_numeric($numero2) ) {
+        return "Error!!";
+    }else{
+        return ($numero1 > $numero2) ?sumAndRest($numero1, $numero2) :multAndDiv($numero1, $numero2);
+    }
 }
 
 try {
     $res = match($_METHOD){
-        "POST" => calcMayor($_DATA),
+        "POST" => calcNumber(...$_DATA),
         default => <<<STRING
         Methodo "${_METHOD}" imposible de ejecutar, rectifique su methodo
         STRING
     };
 } catch (\Throwable $th) {
-    $res = "Error!!";
+    $res = (array)[
+        "sum"=>"Error!!",
+        "rest"=>"Error!!",
+    ];
 }
 
-$respuesta = (array) [
-    "data" => $_DATA,
-    "personaMayor" => $res
-];
+if($res["sum"]){
+    $respuesta = (array) [
+        "data" => $_DATA,
+        "suma" => $res["sum"],
+        "resta" => $res["rest"],
+    ];
+}else{
+    $respuesta = (array) [
+        "data" => $_DATA,
+        "mult" => $res["mult"],
+        "div" => $res["div"],
+    ];
+}
+
 
 echo json_encode($respuesta, JSON_PRETTY_PRINT);
 ?>
--- main.js
+++ main.js
@@ -1,38 +1,25 @@
-// Construir el algoritmo que solicite el nombre y edad de 3
-// personas y determine el nombre de la persona con mayor edad.
+/* Construir el algoritmo que lea por teclado dos números,
+si el primero es mayor al segundo informar su suma y
+diferencia, en caso contrario, informar el producto y la
+división del primero respecto al segundo. */
 
 let myForm = document.querySelector("#myForm");
-let data = [];
 
 myForm.addEventListener("submit", async (e) => {
     e.preventDefault();
     const dataForm = Object.fromEntries(new FormData(e.target));
-    if(dataForm.nombre == "" || dataForm.edad == ""){
+    if(dataForm.numero1 == "" || dataForm.numero2 == ""){
         alert("Error!! Porfavor llenar los campos");
     }else{
-        if(data.length <= 2){
-            data.push(dataForm);
-            alert(dataForm.nombre + " agregado con exito");
-            document.querySelector("#btnContinuar").removeAttribute("disabled");
-            myForm.reset();
-            if(data.length == 3){
-                document.querySelector("#submit").setAttribute("disabled","");
-            }     
-        }else{
-            alert("Ya haz agregado las 3 personas");
-            document.querySelector("#submit").setAttribute("disabled","");
-            myForm.reset();
+        let config = {
+            method: "POST",
+            header: {"Content-Type":"application/json"},
+            body: JSON.stringify(dataForm)
         }
+        let res = await( await fetch("api.php", config)).text();
+        document.querySelector("pre").innerHTML = res;
     }
 });
 
-document.querySelector("#btnContinuar").addEventListener("click", async(e) => {
-    let config = {
-        method: "POST",
-        header: {"Content-Type":"application/json"},
-        body: JSON.stringify(data)
-    }
-    let res = await( await fetch("api.php", config)).text();
-    document.querySelector("pre").innerHTML = res;
-})
+
 
