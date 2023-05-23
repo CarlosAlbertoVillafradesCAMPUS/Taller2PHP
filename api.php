9ef34f6796671fefecde23dc64ab58b4b4683f4e
CarlosAlbertoVillafradesCAMPUS <cavillafrades@gmail.com>
2023-05-22 22:42:39 -0500

    feat(Ejercicios): :rocket: Ejercicio 4
    se desarrollo el ejercicio 4 del taller 2, sobre digitar 3 personas y despues saber cual es la persona mayor. se validaron la gran mayoria anomalias tanto en el frontend como en el backend
    
    by: Carlos Villafrades

--- api.php
+++ api.php
@@ -5,24 +5,43 @@ $_DATA = json_decode(file_get_contents("php://input"), true);
 $_METHOD = $_SERVER["REQUEST_METHOD"];
 var_dump($_METHOD);
 
-function calcVoltaje(float $resistencia, float $intencidad){
-    return $resistencia * $intencidad;    
+function calcMayor($arg){
+    if(count($arg) > 0 && count($arg) <= 3){
+        $position = (int) 0;
+        $sum = (int) 0;
+        $i = (int) 0;
+        foreach($arg as $key) {
+            if (!is_numeric($key["edad"])) {
+                return "Error!!";
+                break;
+                }
+            $edad = (int) $key["edad"];
+            if ($edad >= $sum) {
+                $sum = $edad;
+                $position = $i;
+                $i++;
+            }
+            return $arg[$position];
+        }
+} else{
+    return "Error!!";
+}
 }
 
 try {
     $res = match($_METHOD){
-        "POST" => calcVoltaje(...$_DATA),
+        "POST" => calcMayor($_DATA),
         default => <<<STRING
         Methodo "${_METHOD}" imposible de ejecutar, rectifique su methodo
         STRING
     };
 } catch (\Throwable $th) {
-    $res = "Error";
+    $res = "Error!!";
 }
 
 $respuesta = (array) [
     "data" => $_DATA,
-    "voltaje" => $res
+    "personaMayor" => $res
 ];
 
 echo json_encode($respuesta, JSON_PRETTY_PRINT);
--- main.js
+++ main.js
@@ -1,15 +1,35 @@
-//Construir el algoritmo para determinar el voltaje de un
-//circuito a partir de la resistencia y la intensidad de corriente.
+// Construir el algoritmo que solicite el nombre y edad de 3
+// personas y determine el nombre de la persona con mayor edad.
 
 let myForm = document.querySelector("#myForm");
+let data = [];
+
 myForm.addEventListener("submit", async (e) => {
     e.preventDefault();
     const dataForm = Object.fromEntries(new FormData(e.target));
+    if(data.length <= 2){
+        data.unshift(dataForm);
+        alert(dataForm.nombre + " agregado con exito");
+        document.querySelector("#btnContinuar").removeAttribute("disabled");
+        myForm.reset();
+        if(data.length == 3){
+            document.querySelector("#submit").setAttribute("disabled","");
+        }     
+    }else{
+        alert("Ya haz agregado las 3 personas");
+        document.querySelector("#submit").setAttribute("disabled","");
+        myForm.reset();
+    }
+    
+});
+
+document.querySelector("#btnContinuar").addEventListener("click", async(e) => {
     let config = {
         method: "POST",
         header: {"Content-Type":"application/json"},
-        body: JSON.stringify(dataForm)
+        body: JSON.stringify(data)
     }
     let res = await( await fetch("api.php", config)).text();
     document.querySelector("pre").innerHTML = res;
 })
+
