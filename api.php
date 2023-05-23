3818d4413f286bfb4af3361579e3712a86d06096
CarlosAlbertoVillafradesCAMPUS <cavillafrades@gmail.com>
2023-05-23 17:38:27 -0500

    feat(Ejercicios): :rocket: Ejercicio 7
    se soluciono el ejercicio 7 del taller 2, se realizo una factura en base a un nombre, valor y cantidad de un producto, se siguio validando cualquier anomalia tanto en el frontend como en el backend
    
    by: Carlos Villafrades

--- api.php
+++ api.php
@@ -6,66 +6,49 @@ $_DATA = json_decode(file_get_contents("php://input"), true);
 $_METHOD = $_SERVER["REQUEST_METHOD"];
 var_dump($_METHOD);
 
-function calcMayorNota($arg){
-        $arrayNotas = [];
-        $cantidadHombres= (int) 0;
-        $cantidadMujeres= (int) 0;
-        for ($i=0; $i < count($arg) ; $i++) { 
-            if (!is_string($arg[$i]["nombre"]) || !is_numeric($arg[$i]["nota"])) {
-                return(array)[
-                    "Maxima" => "Error!!",
-                    "Minima" => "Error!!",
-                    "cantHombres" => "Error!!",
-                    "cantMujeres" => "Error!!"
-                ];
-                break;
-            }else{ 
-                $nota = (float)$arg[$i]["nota"];
-                array_push($arrayNotas, $nota);
-                
-                if ($arg[$i]["sexo"] == "M") {
-                    $cantidadHombres++;
-                } else {
-                    $cantidadMujeres++;
-                }
-            } 
-        }
-        $notaMaxima = max($arrayNotas);
-        $notaMinima = min($arrayNotas);
-        $positionNotaMenor = array_search($notaMinima, $arrayNotas);
-        $positionNotaMayor = array_search($notaMaxima, $arrayNotas);
-        var_dump($cantidadHombres);
+function cantTotal(float $cantidad, float $precio){
+    return $cantidad * $precio;
+}
 
+function factura($arg){
+    if (is_numeric($arg["nomProducto"]) || !is_numeric($arg["preArticulo"]) || !is_numeric($arg["canArticulo"])) {
         return(array)[
-            "Maxima" => $arg[$positionNotaMayor],
-            "Minima" => $arg[$positionNotaMenor],
-            "cantHombres" => $cantidadHombres,
-            "cantMujeres" => $cantidadMujeres
+            "Nombre" => "Error!!",
+            "Cantidad" => "Error!!",
+            "Valor" => "Error!!",
+            "Total" => "Error!!"
         ];
+    }else{ 
+        return(array)[
+            "Nombre" => $arg["nomProducto"],
+            "Cantidad" => $arg["canArticulo"],
+            "Valor" => $arg["preArticulo"],
+            "Total" => cantTotal($arg["canArticulo"],$arg["preArticulo"])
+        ];
+    } 
 }
 
 try {
     $res = match($_METHOD){
-        "POST" => calcMayorNota($_DATA),
+        "POST" => factura($_DATA),
         default => <<<STRING
         Methodo "${_METHOD}" imposible de ejecutar, rectifique su methodo
         STRING
     };
 } catch (\Throwable $th) {
     $res = (array)[
-        "Maxima" => "Error!!",
-        "Minima" => "Error!!",
-        "cantHombres" => "Error!!",
-        "cantMujeres" => "Error!!"
+        "Nombre" => "Error!!",
+        "Cantidad" => "Error!!",
+        "Valor" => "Error!!",
+        "Total" => "Error!!"
     ];
 }
 
-
     $respuesta = (array) [
-        "notaMaxima" => $res["Maxima"],
-        "notaMinima" => $res["Minima"],
-        "cantidadHombres" => $res["cantHombres"],
-        "cantidadMujeres" => $res["cantMujeres"]
+        "NombreProducto" => $res["Nombre"],
+        "CantidadProducto" => $res["Cantidad"],
+        "ValorProducto" => $res["Valor"],
+        "TotalALlevar" => $res["Total"]
     ];
 
     echo json_encode($respuesta, JSON_PRETTY_PRINT);
