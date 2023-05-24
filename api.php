b5b68c359d97ea64ee1a53b91c3a3354acf17b73
CarlosAlbertoVillafradesCAMPUS <cavillafrades@gmail.com>
2023-05-24 15:04:12 -0500

    feat(Ejercicios): :rocket: Ejercicio 9
    se soluciono el ejercicio 9 del taller 2. saber cual fue la ganadora de los juegos olimpicos con respecto a sus marcas en salto triple
    
    by:Carlos Villafrades

--- api.php
+++ api.php
@@ -1,70 +1,39 @@
 <?php
-header("Content-Type:application/json");
+    header("Content-Type:application/json");
 
-$_DATA = json_decode(file_get_contents("php://input"), true);
+    $_DATA = json_decode(file_get_contents("php://input"), true);
 
-$_METHOD = $_SERVER["REQUEST_METHOD"];
-var_dump($_METHOD);
+    $_METHOD = $_SERVER["REQUEST_METHOD"];
+    var_dump($_METHOD);
 
-function calcCuadrado($arg){
-    if (is_numeric($arg["valLadoCuadrado"])) {
-        $lado = (float) $arg["valLadoCuadrado"];
-
-        return (array) [
-            "perimetro"=> ($lado * 4)
-        ];
-    }else{
-        return (array) [
-            "perimetro"=> "Error!!"
-        ];
+    function atletaGanadora($arg){
+        foreach ($arg as $value) {
+           if(is_numeric($value["marcaSalto"]) && !is_numeric($value["nombreAtleta"])){
+            $arrayMarcas = array_column($arg, "marcaSalto");
+        $marcaGanadora = max($arrayMarcas);
+        $position = array_search($marcaGanadora, $arrayMarcas);
+        return $arg[$position];
+           } else{
+        return "Erro!!";
+           }
+        }
+        
     }
-}
 
-function calcRectangulo($arg){
-    if (is_numeric($arg["baseRectangulo"]) && is_numeric($arg["alturaRectangulo"])) {
-        $base = (float) $arg["baseRectangulo"];
-        $altura = (float) $arg["alturaRectangulo"];
-
-        return (array) [
-            "area"=> ($base * $altura)
-        ];
-    } else{
-        return (array) [
-            "area"=> "Error!!"
-        ];
+    try {
+        $res = match($_METHOD){
+            "POST" => atletaGanadora($_DATA),
+            default => <<<STRING
+            Methodo "${_METHOD}" imposible de ejecutar, rectifique su methodo
+            STRING
+        };
+    } catch (\Throwable $th) {
+        $res = "Error!!";
     }
-}
 
-function ValidateForm($arg){
-    return ($arg["valLadoCuadrado"]) ?calcCuadrado($arg) :calcRectangulo($arg);
-}
-
-try {
-    $res = match($_METHOD){
-        "POST" => ValidateForm($_DATA),
-        default => <<<STRING
-        Methodo "${_METHOD}" imposible de ejecutar, rectifique su methodo
-        STRING
-    };
-} catch (\Throwable $th) {
-    $res = (array)[
-        "perimetro" => "Error!!",
-        "area" => "Error!!",
+    $respuesta = (array)[
+        "Ganadora"=> $res,
     ];
-}
-
-if($res["perimetro"]){
-    $respuesta = (array) [
-        "data" => $_DATA,
-        "perimetroCuadrado" => $res["perimetro"]
-    ];
-}else{
-    $respuesta = (array) [
-        "data" => $_DATA,
-        "areaRectangulo" => $res["area"]
-    ];
-}
-   
 
     echo json_encode($respuesta, JSON_PRETTY_PRINT);
 ?>
--- main.js
+++ main.js
@@ -3,37 +3,78 @@ si el primero es mayor al segundo informar su suma y
 diferencia, en caso contrario, informar el producto y la
 división del primero respecto al segundo. */
 
-let myFormCuadrado = document.querySelector("#myFormCuadrado");
-let myFormRectangulo = document.querySelector("#myFormRectangulo");
+let myForm = document.querySelector("#myForm");
+let data = [];
 
-myFormCuadrado.addEventListener("submit", async (e) => {
+myForm.addEventListener("submit", (e) =>{
     e.preventDefault();
-    const dataForm = Object.fromEntries(new FormData(e.target));
-    if(dataForm.valLadoCuadrado == ""){
-        alert("Error!! Porfavor llenar los campos");
-    }else{
+    let guardarDatos = Object.fromEntries(new FormData(e.target));
+    
+    if(guardarDatos.cantidadAtletas == ""){
+        alert("Por favor llenar los campos");
+    } else {
+        if(isNaN(guardarDatos.cantidadAtletas)){
+            alert("escriba bien carechimba")
+        } else{
+            let cantidadAtletas = guardarDatos.cantidadAtletas;
+
+            let fielset = document.querySelector("fieldset")
+            fielset.innerHTML = null;
+            fielset.insertAdjacentHTML("beforeend", `
+                                                    <legend>Formulario atletas</legend>
+                                                        <form id="myForm2">
+                                                            <label for="">Digite el nombre de la atleta</label><br>
+                                                            <input type="text" name="nombreAtleta" id="nombreAtleta" required> <br><br>
+    
+                                                            <label for="">Digite la marca en saltos de la atleta</label><br>
+                                                            <input type="text" required pattern="[0-9.]+" name="marcaSalto" id="marcaSalto" required> <br><br>
+                                                        
+                                                            <input type="submit" id="submit" value="Guardar"><br><br>
+                                                            <input type="button" id="btnMostrar" value="Mostrar"><br><br>
+                                                        </form>
+                                                    `);
+                formularioAtletas(cantidadAtletas);
+        }
+       
+    }
+})
+
+function formularioAtletas(arg){
+    let myForm2 = document.querySelector("#myForm2")
+    let record = 15.50;
+    myForm2.addEventListener("submit", async(e) =>{
+        e.preventDefault();
+        let guardarAtletas = Object.fromEntries(new FormData(e.target));
+
+        if(guardarAtletas.nombreAtleta == "" || guardarAtletas.marcaSalto == ""){
+            alert("Error!! Porfavor llenar los campos");
+        }else{
+            if(data.length <= (parseInt(arg)-1)){
+                if(guardarAtletas.marcaSalto > record){
+                    alert(`Felicitaciones ${guardarAtletas.nombreAtleta} acabas de romper el record de  ${record}m con una marca de ${guardarAtletas.marcaSalto}`);
+                    record = guardarAtletas.marcaSalto;
+                    guardarAtletas.recompensa = 500000000;
+                }
+                data.push(guardarAtletas);
+                if(data.length == arg){
+                    document.querySelector("#submit").setAttribute("disabled", ""); 
+                }
+                
+            }else{
+                alert("Cantidad de atletas alcanzada");
+            }
+        }
+        console.log(data);
+        myForm2.reset();
+    })
+
+    document.querySelector("#btnMostrar").addEventListener("click", async(e) =>{
         let config = {
             method: "POST",
             header: {"Content-Type":"application/json"},
-            body: JSON.stringify(dataForm)
+            body: JSON.stringify(data)
         }
         let res = await( await fetch("api.php", config)).text();
-        document.querySelector("pre").innerHTML = res; 
-    }
-});
-
-myFormRectangulo.addEventListener("submit", async (e) => {
-    e.preventDefault();
-    const dataForm = Object.fromEntries(new FormData(e.target));
-    if(dataForm.baseRectangulo == "" || dataForm.alturaRectangulo == ""){
-        alert("Error!! Porfavor llenar los campos");
-    }else{
-        let config = {
-            method: "POST",
-            header: {"Content-Type":"application/json"},
-            body: JSON.stringify(dataForm)
-        }
-        let res = await( await fetch("api.php", config)).text();
-        document.querySelector("pre").innerHTML = res; 
-    }
-});
+        document.querySelector("pre").innerHTML = res;
+    })
+}
